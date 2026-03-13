c91116b4bfa0a707369d402254934554b495fa38
Domenicobrz <domenicobrz@gmail.com>
2026-03-13 02:15:47 +0100

    updated playground test with uniforms

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,19 +1,23 @@
-import { struct, volten, Buffer, Kernel } from "@volten/core";
+import { struct, volten, Buffer, Kernel, Uniform } from '@volten/core';
 
 const v = await volten();
 
 // Buffer has 8 elements, but we only process the first 4
-const buf = new Buffer([1, 2, 3, 4, 100, 100, 100, 100], "f32", "rw");
+const buf = new Buffer([1, 2, 3, 4, 100, 100, 100, 100], 'f32', 'rw');
+const mult = new Uniform(10, 'f32');
+
 const k = new Kernel(
-  `
+    `
         fn main(gid: vec3u) {
-          inout[gid.x] = inout[gid.x] * 10.0;
+          inout[gid.x] = inout[gid.x] * mult;
         }
       `,
-  { threads: 4 },
+    { threads: 4 }
 );
-const node = v.pass(k, { inout: buf });
+const node = v.pass(k, { inout: buf, mult });
+
 v.run(node);
+
 const output = await v.read(node);
 
 console.log(Array.from(output.inout));
