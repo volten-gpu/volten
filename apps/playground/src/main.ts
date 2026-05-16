e0ed2d14a00f56108b04f7eb67e9bb938814636a
Domenicobrz <domenicobrz@gmail.com>
2026-05-16 16:13:48 +0200

    updated main

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -2,48 +2,19 @@ import { volten, Buffer, Kernel, Uniform } from '@volten/core';
 
 async function main(): Promise<void> {
     const v = await volten();
-
-    // Buffer has 8 elements, but we only process the first 4
-    const buf = new Buffer([1, 2, 3, 4, 100, 100, 100, 100], 'f32', 'rw');
+    const inout = new Buffer([1, 2, 3, 4], 'f32', 'rw');
     const mult = new Uniform(10, 'f32');
 
-    const k = new Kernel(
-        `
-        fn main(gid: vec3u) {
-          if (gid.x == 2) {
-            enableDebug();
-            debugF32("test message", inout[gid.x] * mult);
-          }       
-               
-          if (gid.x == 1) {
-            enableDebug();
-            debugVec4f("test message 2", vec4f(0.0, 1.0, 2.0, 3.0));
-          }
+    const kernel = new Kernel(`
+      fn main(gid: vec3u) {
+        inout[gid.x] = inout[gid.x] * mult;
+      }
+    `);
 
-          inout[gid.x] = inout[gid.x] * mult;
-        }
-      `,
-        {
-            threads: 4,
-            label: 'Ktest'
-        }
-    );
-
-    const A = v.pass(k, { inout: buf, mult }, { debug: true });
-    // const B = v.pass(k, { inout: buf, mult });
-
-    // v.run([A, B]);
+    const A = v.pass(kernel, { inout, mult });
     v.run(A);
 
-    const output = await v.read(buf);
-    const output2 = await v.read(A.inout);
-    console.log(Array.from(output));
-    console.log(output2);
-
-    const debugRes = await v.readDebug(A);
-    debugRes.print();
+    console.log(await v.read(inout));
 }
 
-main().catch((error) => {
-    console.error(error);
-});
+main();
